ee6784c4b1346c43ce1bc82e042a2e6afc84dd47
Lucas Krause <dev@futape.de>
2018-01-23 13:04:09 +0100

    FEATURE: Implement info window
    Implements an info window showing the locations' titles and images.

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -7,12 +7,46 @@
 		map = new maps.Map($('#map').get(0), {
 			center: bremen,
 			zoom: zoom
+		}),
+		info = new maps.InfoWindow(),
+		globalDummyMarker = new maps.Marker({
+			position: new maps.LatLng(90 * 2, 180 * 2, true),
+			map: map,
+			clickable: false,
+			cursor: 'auto'
 		});
 
 	map.data.setStyle(function(feature) {
 		return {
 			title: feature.getProperty('title')
 		}
+	});
+
+	map.data.addListener('click', function(e) {
+		var position = null;
+		e.feature.getGeometry().forEachLatLng(function(latLng) {
+			if (position === null) {
+				position = latLng;
+			}
+		});
+		var dummyMarker = new maps.MVCObject();
+		dummyMarker.set('position', position);
+		dummyMarker.set('anchorPoint', globalDummyMarker.get('anchorPoint'));
+
+		var content = $('<div></div>');
+		$('<div></div>').css('font-weight', 'bold').text(e.feature.getProperty('title')).appendTo(content);
+		if (typeof e.feature.getProperty('image') != 'undefined') {
+			$('<img />').prop({
+				src: e.feature.getProperty('image'),
+				alt: e.feature.getProperty('title')
+			}).css({
+				'max-width': '100%',
+				'margin-top': '.5em'
+			}).appendTo(content);
+		}
+		info.setContent(content.get(0));
+
+		info.open(map, dummyMarker);
 	});
 
 	map.data.loadGeoJson('/resources/data/locations.json');
